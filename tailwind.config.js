8d0ad6aa3addced1a870fdabde5aa3673830ba8f
Polat OSMANOĞLU <48163610+osmanoglupolat@users.noreply.github.com>
2024-12-24 00:16:26 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,22 @@ module.exports = {
   content: [
     './src/**/*.pug',
   ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1600px'
+      }
+    },
+    extend: {
+      colors: {
+        primary: '#002444',
+      },
+      aspectRatio: {
+        '1920/1080': '1920/1080'
+      }
+    },
+  },
+  plugins: [],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = {
         test: /\.css$/,
         use: ["css-loader", "postcss-loader"],
       },
+      {
+        test: /\.(ico|png|jp?g|webp|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[hash:8][ext][query]',
+        },
+      },
     ],
   },
 
